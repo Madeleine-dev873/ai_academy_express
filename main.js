53dde2cf99077c9a7d43a447bb54354ae2f68b5e
Madeleine-dev873 <madeleinedembakadidiadiop@esp.sn>
2025-05-04 17:21:49 +0000

    Commit des modifications pour le Lab 9

--- main.js
+++ main.js
@@ -1,48 +1,97 @@
 const express = require("express");
 const layouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const errorController = require("./Controllers/errorController");
-const homeController = require("./Controllers/homeController");  // Ajoutez l'importation du contrôleur
-const subscribersController = require("./Controllers/subscribersController");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
 
-// Connexion à MongoDB
-mongoose.connect("mongodb://localhost:27017/ai_academy", { useNewUrlParser: true });
+const homeController = require("./controllers/homeController");
+const errorController = require("./controllers/errorController");
+const subscribersController = require("./controllers/subscribersController");
+const usersController = require("./Controllers/usersController");
+const coursesController = require("./controllers/coursesController");
+const authController = require("./Controllers/authController");
+
+const app = express();
+
+// Configuration de la connexion à MongoDB
+mongoose.connect("mongodb://localhost:27017/ai_academy", {
+  useNewUrlParser: true
+});
 
 const db = mongoose.connection;
 db.once("open", () => {
   console.log("Connexion réussie à MongoDB en utilisant Mongoose!");
 });
 
-const app = express();
-
-// Configuration du moteur de templates
+// Configuration de l'application
+app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
-
-// Middleware
-app.use(layouts);
 app.use(express.static("public"));
+app.use(layouts);
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method", {
+  methods: ["POST", "GET"]
+}));
 
-// Routes
-app.get("/", homeController.index);  // Utilisez le contrôleur homeController pour la route d'accueil
-app.get("/about", homeController.about);  // Exemple d'utilisation de la route 'about'
-app.get("/courses", homeController.courses);  // Exemple d'utilisation de la route 'courses'
-app.get("/contact", homeController.contact);  // Exemple d'utilisation de la route 'contact'
-app.post("/contact", homeController.processContact);  // Traiter le formulaire de contact
+// Configuration des cookies et des sessions
+app.use(cookieParser("secret_passcode"));
+app.use(session({
+  secret: "secret_passcode",
+  cookie: { maxAge: 4000000 },
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Configuration de flash messages
+app.use(flash());
+
+// Configuration de Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configuration du User model pour Passport
+const User = require("./models/user");
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Middleware global
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  res.locals.loggedIn = req.isAuthenticated();
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// Routes d'authentification
+app.get("/login", authController.login);
+app.post("/login", authController.authenticate);
+app.get("/logout", authController.logout, usersController.redirectView);
+app.get("/signup", authController.signup);
+app.post("/signup", authController.register, usersController.redirectView);
+
+// Routes protégées
+app.use("/users", authController.ensureLoggedIn);
+app.use("/courses/new", authController.ensureLoggedIn);
+app.use("/courses/:id/edit", authController.ensureLoggedIn);
+
+// Routes générales
+app.get("/", homeController.index);
+app.get("/about", homeController.about);
+app.get("/courses", homeController.courses);
+app.get("/contact", homeController.contact);
+app.post("/contact", homeController.processContact);
+
 // Routes pour les abonnés
 app.get("/subscribers", subscribersController.getAllSubscribers);
 app.get("/subscribers/new", subscribersController.getSubscriptionPage);
 app.post("/subscribers/create", subscribersController.saveSubscriber);
 app.get("/subscribers/:id", subscribersController.show);
-// Ajoutez les contrôleurs
-const usersController = require("./Controllers/usersController");
-const coursesController = require("./controllers/coursesController");
-// Ajouter le middleware method-override
-const methodOverride = require("method-override");
-app.use(methodOverride("_method", {
-methods: ["POST", "GET"]
 
-}));
 // Routes pour les utilisateurs
 app.get("/users", usersController.index, usersController.indexView);
 app.get("/users/new", usersController.new);
@@ -51,6 +100,7 @@ app.get("/users/:id", usersController.show, usersController.showView);
 app.get("/users/:id/edit", usersController.edit);
 app.put("/users/:id/update", usersController.update, usersController.redirectView);
 app.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
+
 // Routes pour les cours
 app.get("/courses", coursesController.index, coursesController.indexView);
 app.get("/courses/new", coursesController.new);
@@ -64,7 +114,7 @@ app.delete("/courses/:id/delete", coursesController.delete, coursesController.re
 app.use(errorController.pageNotFound);
 
 // Lancer le serveur
-const port = 3000;
+const port = app.get("port");
 app.listen(port, () => {
   console.log(`🚀 Serveur lancé sur http://localhost:${port}`);
 });
