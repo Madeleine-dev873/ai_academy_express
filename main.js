f27e723b7cdaeacff6fdd2155e38cdeb475625c6
Madeleine-dev873 <madeleinedembakadidiadiop@esp.sn>
2025-05-04 15:11:30 +0000

    Ajout des fonctionnalités du Lab8

--- main.js
+++ main.js
@@ -34,6 +34,31 @@ app.get("/subscribers", subscribersController.getAllSubscribers);
 app.get("/subscribers/new", subscribersController.getSubscriptionPage);
 app.post("/subscribers/create", subscribersController.saveSubscriber);
 app.get("/subscribers/:id", subscribersController.show);
+// Ajoutez les contrôleurs
+const usersController = require("./Controllers/usersController");
+const coursesController = require("./controllers/coursesController");
+// Ajouter le middleware method-override
+const methodOverride = require("method-override");
+app.use(methodOverride("_method", {
+methods: ["POST", "GET"]
+
+}));
+// Routes pour les utilisateurs
+app.get("/users", usersController.index, usersController.indexView);
+app.get("/users/new", usersController.new);
+app.post("/users/create", usersController.create, usersController.redirectView);
+app.get("/users/:id", usersController.show, usersController.showView);
+app.get("/users/:id/edit", usersController.edit);
+app.put("/users/:id/update", usersController.update, usersController.redirectView);
+app.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
+// Routes pour les cours
+app.get("/courses", coursesController.index, coursesController.indexView);
+app.get("/courses/new", coursesController.new);
+app.post("/courses/create", coursesController.create, coursesController.redirectView);
+app.get("/courses/:id", coursesController.show, coursesController.showView);
+app.get("/courses/:id/edit", coursesController.edit);
+app.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
+app.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
 
 // Gestion des erreurs
 app.use(errorController.pageNotFound);
